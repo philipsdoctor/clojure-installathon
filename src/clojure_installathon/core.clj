610994ebfcaac573e0d3f8376c11d8a73a2583bb
Philip S Doctor <diomedestydeus@gmail.com>
2014-06-15 21:00:06 +0000

    Lein new clojure-installathon

---
+++ src/clojure_installathon/core.clj
@@ -0,0 +1,1 @@
+(ns clojure-installathon.core)
---
+++ test/clojure_installathon/test/core.clj
@@ -0,0 +1,6 @@
+(ns clojure-installathon.test.core
+  (:use [clojure-installathon.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
