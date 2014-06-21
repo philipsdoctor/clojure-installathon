908ef9c7140555c4755c634b5cf7ded0bd1e8b3a
Philip S Doctor <diomedestydeus@gmail.com>
2014-06-21 17:21:07 +0000

    a solution for problem 56

--- src/clojure_installathon/core.clj
+++ src/clojure_installathon/core.clj
@@ -13,5 +13,18 @@
 
 (defn distinct-item
   "Removes duplicate items from a sequence, preserving order"
-  [repeat-sequence]
-  repeat-sequence)
+  [repeat-collection]
+  (loop [current-collection repeat-collection
+         return-collection []]
+    (if (empty? current-collection)
+      return-collection
+      (if (contains? (set return-collection) (first current-collection))
+        (recur (rest current-collection) return-collection)
+        (recur (rest current-collection)
+               (conj return-collection (first current-collection)))))))
+
+(defn recursive
+  "a recursive function that will overflow"
+  [seq current-iteration]
+    (prn current-iteration)
+  (recursive seq (inc current-iteration)))
